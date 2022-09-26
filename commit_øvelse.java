a11b18fcb208cd7e9b395a036e96fc01a6fb515d
UffeBC <113894934+UffeBC@users.noreply.github.com>
2022-09-26 16:59:13 +0200

    hvadskerder

--- commit_øvelse.java
+++ commit_øvelse.java
@@ -1,5 +1,6 @@
 public class commit_øvelse {
     public static void main(String[] args) {
+        System.out.println("hvadskerder");
         int a = 2 + 2;
     System.out.println(a);
     System.out.println("2");
