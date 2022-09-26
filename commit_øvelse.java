e37bda82881faf07d5ee1efbfd593cd0ef740cc1
UffeBC <113894934+UffeBC@users.noreply.github.com>
2022-09-26 16:58:11 +0200

    hej master

--- commit_øvelse.java
+++ commit_øvelse.java
@@ -1,7 +1,7 @@
 public class commit_øvelse {
     public static void main(String[] args) {
         int a = 2 + 2;
-    System.out.println(a);
+    //hey master
     System.out.println("2");
     int ten = 10;
     System.out.println("45");
